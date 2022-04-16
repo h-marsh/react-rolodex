c068e56f904fd39d6e7a3de5932e0fe2cc15a79d
Hunter Marsh <marshsh@gmail.com>
2022-04-16 17:41:15 -0400

    appjs is the 'main' react file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React!! Or else...
+					Learn React!! And JS for that matter lol.
 				</a>
 			</header>
 		</div>
