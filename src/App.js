8ce197a28201cdc2b36360e09d30fa877c758247
Hunter Marsh <marshsh@gmail.com>
2022-06-03 20:50:22 -0400

    small optimizations: declaring a method in the class itself instead of using it as an anonymous function in the onChange callback.  used destructuring to get variables from the state and the method from the class to avoid needing to use the 'this' keyword over and over

--- src/App.js
+++ src/App.js
@@ -24,11 +24,22 @@ class App extends Component {
 		});
 	}
 
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLocaleLowerCase();
+
+		this.setState(() => {
+			return { searchField };
+		});
+	};
+
 	render() {
 		console.log('render');
 
-		const filteredMonsters = this.state.monsters.filter((monster) => {
-			return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
 		});
 
 		return (
@@ -37,13 +48,7 @@ class App extends Component {
 					className="search-box"
 					type="search"
 					placeholder="search monsters"
-					onChange={(event) => {
-						const searchField = event.target.value.toLocaleLowerCase();
-
-						this.setState(() => {
-							return { searchField };
-						});
-					}}
+					onChange={onSearchChange}
 				/>
 				{filteredMonsters.map((monster) => {
 					return <h1 key={monster.id}>{monster.name}</h1>;
