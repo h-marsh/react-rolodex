8effdc994563478e2f27050194ddeac9768cb424
Hunter Marsh <marshsh@gmail.com>
2022-07-16 21:31:21 -0400

    some more css in react

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,10 +5,20 @@ class CardList extends Component {
 	render() {
 		const { monsters } = this.props;
 		return (
-			<div>
-				{monsters.map((monster) => (
-					<h1>{monster.name}</h1>
-				))}
+			<div className="card-list">
+				{monsters.map((monster) => {
+					const { name, id, email } = monster;
+					return (
+						<div className="card-container" key={id}>
+							<img
+								alt={`monster: ${name}`}
+								src={`https://robohash.org/${id}?set=set2&size=180x180`}
+							/>
+							<h2>{name}</h2>
+							<p>{email}</p>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
